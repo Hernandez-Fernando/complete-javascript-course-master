fa7b5d9d72cc24450f83a67b8d98f888dadfe2a0
Fernando Hernandez <chivista3001@icloud.com>
2022-01-10 20:29:35 -0700

    Added Assignment 4 & 5

--- 01-Fundamentals-Part-1/starter/assignments.js
+++ 01-Fundamentals-Part-1/starter/assignments.js
@@ -19,4 +19,11 @@ console.log(typeof(language));
 // language = "Spanish";
 // Shows error
 
-//Assignment 4
+//Assignment 4 & 5
+console.log(population / 2);
+console.log(population + 1);
+console.log(population > 6);
+console.log(population > 33);
+
+let description = `${country} is in ${continent}, and its ${population} millon people speak ${language}`;
+console.log(description);
