e5293e62594ad1fe476449a13dce8297cf3fbc72
Fernando Hernandez <Hernandez-Fernando@users.noreply.github.com>
2022-02-12 12:17:47 -0700

    Updated
    Added Assignment 9 & Coding Challenge 3

--- 01-Fundamentals-Part-1/starter/assignments.js
+++ 01-Fundamentals-Part-1/starter/assignments.js
@@ -52,3 +52,11 @@ if (numNeighbours === 1) {
 } else {
     console.log('No borders');
 } */
+
+
+// Assignement 9
+if (language === 'English' && population < 50 && !isIsland) {
+	console.log(`You should live in ${country}.`);
+} else {
+	console.log(`${country} does not meet your criteria.`);
+}
--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -16,3 +16,17 @@ if (markHigherBMI == 1) {
 } else {
     console.log(`John's BMI (${johnBMI}) is higher than Mark's (${markBMI})!`);
 }
+
+// Code Challenge 3
+const dolphinsAverage = (97 + 112 + 101) / 3;
+const koalasAverage = (109 + 95 + 106) / 3;
+
+if (dolphinsAverage > koalasAverage && dolphinsAverage >= 100) {
+	console.log('Dolphins Win Trophy');
+} else if (dolphinsAverage === koalasAverage && dolphinsAverage >= 100) {
+	console.log('Tie, both teams scored equal. No one get trophy.');
+} else if (koalasAverage >= 100) {
+	console.log('Koalas Win Trophy');
+} else {
+	console.log('No team scored the minimum points to win.');
+}
